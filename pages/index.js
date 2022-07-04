ac83ab91e17d9089dd0eb54048966fac607044ef
Daniil <dadyarri@gmail.com>
2022-07-04 20:36:20 +0300

    add avatar; remove some shit

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,32 @@
-import { Box, Container, Heading } from '@chakra-ui/react';
+import {Box, Container, Heading, Image, useColorModeValue} from '@chakra-ui/react';
 
 const Page = () => {
   return (
     <Container>
-      <Box
-        borderRadius="lg"
-        bg="blackAlpha.900"
-        color="white"
-        p={3}
-        mb={6}
-        align="center"
-      >
-        Привет, я Даниил, бэкенд-разработчик из Владимира.
-      </Box>
-
       <Box display={{ md: 'flex' }}>
         <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Даниил Голубев
           </Heading>
+          <p>Бэкенд-разработчик, фанат Jetbrains</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          align={'center'}
+        >
+          <Image
+            borderColor={'whiteAlpha.800'}
+            borderWidth={2}
+            borderStyle={'solid'}
+            width={100}
+            height={100}
+            display={'inline-block'}
+            borderRadius={'full'}
+            src={'/images/avatar.jpg'}
+            marginTop={3}
+          />
         </Box>
       </Box>
     </Container>
