b4bd78204b60fb445986cea24ca013921cf69304
Daniil <dadyarri@gmail.com>
2022-07-04 19:36:52 +0300

    reformat code

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import Main from '../components/layouts/main';
-import theme from "../libs/theme";
+import theme from '../libs/theme';
 
 const Website = ({ Component, pageProps, router }) => {
   return (
