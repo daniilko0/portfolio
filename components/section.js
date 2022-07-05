4609001031e46eaec234d7adc3a2f7d467f1c512
Daniil <dadyarri@gmail.com>
2022-07-05 10:00:07 +0300

    fix animation delay

--- components/section.js
+++ components/section.js
@@ -11,7 +11,8 @@ const Section = ({ children, delay = 0 }) => (
   <StyledDiv
     initial={{ y: 10, opacity: 0 }}
     animate={{ y: 0, opacity: 1 }}
-    transition={{ duration: 0.8, delay: delay }}
+    transitionDuration={'0.8s'}
+    transitionDelay={delay.toString() + 's'}
     mb={6}
   >
     {children}
