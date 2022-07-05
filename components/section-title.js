bd2948a00087a90c5dbbf1f730a9b2e1fba2481e
Daniil <dadyarri@gmail.com>
2022-07-05 09:26:11 +0300

    add component for section title

---
+++ components/section-title.js
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const SectionTitle = styled.h3`
+  text-decoration: underline;
+  font-size: 20px;
+  text-underline-offset: 6px;
+  text-decoration-color: #525252;
+  text-decoration-thickness: 4px;
+  margin-top: 3px;
+  margin-bottom: 10px;
+`;
+
+export default SectionTitle;
