7531883c7753aea6dbca3aeaa601a8b900c7ded8
Daniil <dadyarri@gmail.com>
2022-08-01 20:30:43 +0300

    increase size of heading

--- components/section-title.js
+++ components/section-title.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 const SectionTitle = styled.h3`
   text-decoration: underline;
-  font-size: 20px;
+  font-size: 24px;
   text-underline-offset: 6px;
   text-decoration-color: #525252;
   text-decoration-thickness: 4px;
