3584f5513b6f8ac3f59c888f9a18ac67c2580dc2
Daniil <dadyarri@gmail.com>
2022-08-01 20:30:17 +0300

    fix theme

--- libs/theme.js
+++ libs/theme.js
@@ -1,49 +1,31 @@
 import { mode } from '@chakra-ui/theme-tools';
 import { extendTheme } from '@chakra-ui/react';
-
-const styles = {
-  global: props => ({
-    body: {
-      bg: mode('#f0e7db', '#202023')(props)
-    }
-  })
-};
-
-const components = {
-  Heading: {
-    variants: {
-      'section-title': {
-        textDecoration: 'underline',
-        fontSize: 20,
-        textUnderlineOffset: 6,
-        textDecorationColor: '#525252',
-        textDecorationThickness: 4,
-        marginTop: 3,
-        marginBottom: 4
+const theme = extendTheme({
+  config: {
+    initialColorMode: 'dark',
+    useSystemColorMode: true
+  },
+  styles: {
+    global: {
+      body: {
+        bg: mode('#f0e7db', '#202023')
       }
     }
   },
-  Link: {
-    baseStyle: props => ({
-      color: mode('#3d7aed', '#ff63c3')(props),
-      textUnderlineOffset: 3
-    })
+  components: {
+    Link: {
+      baseStyle: {
+        color: mode('#3d7aed', '#ff63c3'),
+        textUnderlineOffset: 3
+      }
+    }
+  },
+  colors: {
+    glassTeal: '#88ccca'
+  },
+  fonts: {
+    headings: "'Bellota'"
   }
-};
-
-const colors = {
-  glassTeal: '#88ccca'
-};
-
-const fonts = {
-  headings: "'Bellota'"
-};
-
-const config = {
-  initialColorMode: 'dark',
-  useSystemColorMode: true
-};
-
-const theme = extendTheme(config, styles, components, colors, fonts);
+});
 
 export default theme;
