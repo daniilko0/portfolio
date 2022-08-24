c6586e5a2512c591185852c4ba997cb534434c09
Daniil <dadyarri@gmail.com>
2022-08-24 15:54:49 +0300

    rewrite bio components without emotion

---
+++ components/bio.tsx
@@ -0,0 +1,14 @@
+import {Box} from "@chakra-ui/react";
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+}
+
+export const BioSection = ({ children }: Props) => (
+    <Box style={{paddingLeft: "3.4em", textIndent: "-3.4em"}}>{children}</Box>
+);
+
+export const BioYear = ({ children }: Props) => (
+    <span style={{fontWeight: "bold", marginRight: "1em"}}>{children}</span>
+);
