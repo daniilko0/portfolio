92bcd6b38c19537491e2449118128e1120bc10b3
Daniil <dadyarri@gmail.com>
2022-08-30 12:19:49 +0300

    add variant tbw, which grayscales preview and disables link

--- components/posts-grid-item.tsx
+++ components/posts-grid-item.tsx
@@ -6,26 +6,51 @@ type Props = {
   title: string;
   href: string;
   thumbnails: [string, string];
+  variant?: "default" | "tbw";
 };
 
-export const PostsGridItem = ({ children, title, href, thumbnails }: Props) => {
+export const PostsGridItem = ({
+  children,
+  title,
+  href,
+  thumbnails,
+  variant = "default"
+}: Props) => {
   return (
     // @ts-ignore 2322
     <Box w={"100%"} align={"center"}>
       <LinkBox cursor={"pointer"}>
-        <Image
-          src={thumbnails[0]}
-          fallbackSrc={thumbnails[1]}
-          alt={title}
-          className={"grid-item-thumbnail"}
-          loading={"lazy"}
-          fallback={<Spinner />}
-          width={320}
-          height={200}
-        />
-        <LinkOverlay href={href} target={"_blank"}>
+        {variant == "default" ? (
+          <Image
+            src={thumbnails[0]}
+            fallbackSrc={thumbnails[1]}
+            alt={title}
+            style={{ borderRadius: "12px" }}
+            loading={"lazy"}
+            fallback={<Spinner />}
+            width={320}
+            height={200}
+          />
+        ) : variant == "tbw" ? (
+          <Image
+            src={thumbnails[0]}
+            fallbackSrc={thumbnails[1]}
+            alt={title}
+            style={{ borderRadius: "12px", filter: "grayscale(100%)" }}
+            loading={"lazy"}
+            fallback={<Spinner />}
+            width={320}
+            height={200}
+          />
+        ) : null}
+
+        {variant == "default" ? (
+          <LinkOverlay href={href} target={"_blank"}>
+            <Text mt={2}>{title}</Text>
+          </LinkOverlay>
+        ) : variant == "tbw" ? (
           <Text mt={2}>{title}</Text>
-        </LinkOverlay>
+        ) : null}
       </LinkBox>
       <Text fontSize={14}>{children}</Text>
     </Box>
