7c7b442c8dc9d8b8af935a2892a69344b4010ce5
Daniil <dadyarri@gmail.com>
2023-01-22 14:22:14 +0300

    update description of project; update thumbnails

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -5,74 +5,77 @@ import {ProjectsGridItem} from "../components/projects-grid-item";
 import React from "react";
 
 const Projects = () => {
-  return (
-    <Layout title={"Проекты"}>
-      <Container my={5}>
-        <Heading as={"h2"} variant={"page-title"} mb={4}>
-          Проекты
-        </Heading>
+    return (
+        <Layout title={"Проекты"}>
+            <Container my={5}>
+                <Heading as={"h2"} variant={"page-title"} mb={4}>
+                    Проекты
+                </Heading>
 
-        <Section>
-          <Heading as={"h3"} variant={"sectionTitle"}>
-            Личные
-          </Heading>
-          <SimpleGrid columns={[1, 1, 2]} gap={6}>
-            <ProjectsGridItem
-              id={"choco"}
-              title={"ChocoManager"}
-              labels={[
-                "Python",
-                "FastAPI",
-                "C#",
-                "ASP.NET",
-                "EF Core",
-                "Docker Compose",
-                "PostgreSQL",
-                "VK API",
-                "Telegram API"
-              ]}
-              thumbnails={[
-                "/images/projects/choco/webp/common/thumbnail.webp",
-                "/images/projects/choco/png/common/thumbnail.png"
-              ]}
-            >
-              Система работы с клиентами и остатками в интернет-магазине
-            </ProjectsGridItem>
-            <ProjectsGridItem
-              id={"cv"}
-              title={"Сайт-резюме"}
-              labels={["React", "Next.js", "Typescript"]}
-              thumbnails={[
-                "/images/projects/cv/webp/common/thumbnail.webp",
-                "/images/projects/cv/png/common/thumbnail.png"
-              ]}
-            >
-              Этот сайт с резюме и перечнем реализованных проектов
-            </ProjectsGridItem>
-          </SimpleGrid>
-        </Section>
-        <Section>
-          <Heading as={"h3"} variant={"sectionTitle"}>
-            Работа в команде
-          </Heading>
-          <SimpleGrid columns={[1, 1, 2]} gap={6}>
-            <ProjectsGridItem
-              id={"star"}
-              title={"Student Archive"}
-              labels={["Java", "Spark framework", "JDBC", "PostgreSQL"]}
-              thumbnails={[
-                "/images/projects/star/webp/thumbnail.webp",
-                "/images/projects/star/png/thumbnail.png"
-              ]}
-            >
-              Приложение для архивирования полезных учебных материалов
-              (лабораторные работы, методические материалы и пр.)
-            </ProjectsGridItem>
-          </SimpleGrid>
-        </Section>
-      </Container>
-    </Layout>
-  );
+                <Section>
+                    <Heading as={"h3"} variant={"sectionTitle"}>
+                        Личные
+                    </Heading>
+                    <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                        <ProjectsGridItem
+                            id={"choco"}
+                            title={"ChocoManager"}
+                            labels={[
+                                "Python",
+                                "FastAPI",
+                                "C#",
+                                "ASP.NET",
+                                "EF Core",
+                                "Docker",
+                                "Docker Compose",
+                                "PostgreSQL",
+                                "VK API",
+                                "Telegram API",
+                                "Javascript",
+                                "React"
+                            ]}
+                            thumbnails={[
+                                "/images/projects/choco/webp/common/thumbnail.webp",
+                                "/images/projects/choco/png/common/thumbnail.png"
+                            ]}
+                        >
+                            Система работы с клиентами и остатками в интернет-магазине
+                        </ProjectsGridItem>
+                        <ProjectsGridItem
+                            id={"cv"}
+                            title={"Сайт-резюме"}
+                            labels={["React", "Next.js", "Typescript"]}
+                            thumbnails={[
+                                "/images/projects/cv/webp/common/thumbnail.webp",
+                                "/images/projects/cv/png/common/thumbnail.png"
+                            ]}
+                        >
+                            Этот сайт с резюме и перечнем реализованных проектов
+                        </ProjectsGridItem>
+                    </SimpleGrid>
+                </Section>
+                <Section>
+                    <Heading as={"h3"} variant={"sectionTitle"}>
+                        Работа в команде
+                    </Heading>
+                    <SimpleGrid columns={[1, 1, 2]} gap={6}>
+                        <ProjectsGridItem
+                            id={"star"}
+                            title={"Student Archive"}
+                            labels={["Java", "Spark framework", "JDBC", "PostgreSQL"]}
+                            thumbnails={[
+                                "/images/projects/star/webp/thumbnail.webp",
+                                "/images/projects/star/png/thumbnail.png"
+                            ]}
+                        >
+                            Приложение для архивирования полезных учебных материалов
+                            (лабораторные работы, методические материалы и пр.)
+                        </ProjectsGridItem>
+                    </SimpleGrid>
+                </Section>
+            </Container>
+        </Layout>
+    );
 };
 
 export default Projects;
