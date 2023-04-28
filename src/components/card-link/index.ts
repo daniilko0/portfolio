7497ba72b6dec1eb1cbfbda93fa9bb3e5d3e51cc
Daniil <dadyarri@gmail.com>
2023-04-28 21:26:15 +0300

    feat: add component for card links

---
+++ src/components/card-link/index.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const CardLink = styled.a(() => ({
+    fontFamily: ["Fira Code", "monospace"],
+    color: "black",
+    fontSize: "18px",
+    fontWeight: "700",
+    textDecoration: "none"
+}))
