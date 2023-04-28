c55e97dc467cb421d0b7d4f93278c3b000177d84
Daniil <dadyarri@gmail.com>
2023-04-28 14:02:26 +0300

    feat: add menu & menu item components

---
+++ src/components/menu-item/index.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const MenuItem = styled.a(() => ({
+    fontFamily: ["Fira Code", "monospace"],
+    fontSize: "24px",
+    color: "black",
+    textDecoration: "none",
+    padding: "0px 10px"
+}))
