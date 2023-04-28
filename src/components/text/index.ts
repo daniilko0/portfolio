d081063852b86252afe35b86d8578afbbdde80c8
Daniil <dadyarri@gmail.com>
2023-04-28 21:12:57 +0300

    design: decrease margins of text

--- src/components/text/index.ts
+++ src/components/text/index.ts
@@ -2,5 +2,6 @@ import styled from "@emotion/styled";
 
 export const Text = styled.p(() => ({
     fontFamily: ["Fira Code", "monospace"],
-    fontSize: 20
+    fontSize: 20,
+    margin: "10px 0px"
 }))
