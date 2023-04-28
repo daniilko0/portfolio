76759961b49abce2c2262869e043f32a3f164fe2
Daniil <dadyarri@gmail.com>
2023-04-28 14:23:15 +0300

    design: add variant prop to heading

--- src/components/heading/index.ts
+++ src/components/heading/index.ts
@@ -1,8 +1,12 @@
 import styled from "@emotion/styled";
 
-export const Heading = styled.h1(() => ({
-    fontSize: 30,
-    fontWeight: 700,
+interface HeadingProps {
+    variant: "h1" | "h2"
+}
+
+export const Heading = styled.h1((props: HeadingProps) => ({
+    fontSize: props.variant == "h1" ? 30 : 24,
+    fontWeight: props.variant == "h1" ? 700 : 400,
     margin: 0,
     fontFamily: ["Fira Code", "monospace"]
 }))
