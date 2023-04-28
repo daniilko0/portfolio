fd62507a9ba376a513fcf1be06b07ce993ea80de
Daniil <dadyarri@gmail.com>
2023-04-28 14:28:23 +0300

    feat: add some content to home block

--- src/blocks/home/index.tsx
+++ src/blocks/home/index.tsx
@@ -1,3 +1,9 @@
+import {Heading} from "../../components/heading";
+import {Block} from "../../components/block";
+
 export const Home = () => {
-    return <></>
+    return <Block>
+        <Heading variant={"h1"} style={{padding: 5}}>Даниил Голубев</Heading>
+        <Heading variant={"h2"} style={{padding: 5}}>Бэкенд-разработчик</Heading>
+    </Block>
 }
