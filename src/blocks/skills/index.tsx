1c4469993f853867c231f711bd525450066f28b5
Daniil <dadyarri@gmail.com>
2023-04-28 16:16:53 +0300

    feat: add ids to blocks

--- src/blocks/skills/index.tsx
+++ src/blocks/skills/index.tsx
@@ -2,7 +2,7 @@ import {Heading} from "../../components/heading";
 import {Block} from "../../components/block";
 
 export const Skills = () => {
-    return <Block>
+    return <Block id={"skills"}>
         <Heading variant={"h1"}>Навыки</Heading>
     </Block>
 }
