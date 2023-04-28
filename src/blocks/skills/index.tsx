0680ee2526410374b9238ffbb37bffb48eaabccf
Daniil <dadyarri@gmail.com>
2023-04-28 16:13:10 +0300

    refactor: remove fullpage.js

--- src/blocks/skills/index.tsx
+++ src/blocks/skills/index.tsx
@@ -2,7 +2,7 @@ import {Heading} from "../../components/heading";
 import {Block} from "../../components/block";
 
 export const Skills = () => {
-    return <Block className={"section"}>
+    return <Block>
         <Heading variant={"h1"}>Навыки</Heading>
     </Block>
 }
