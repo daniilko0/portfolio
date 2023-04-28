09aaf01b512f8d7ddbaadaaa2ac126cef0fc98eb
Daniil <dadyarri@gmail.com>
2023-04-28 14:02:14 +0300

    feat: add heading component

---
+++ src/components/heading.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+import "@fontsource/fira-code/700.css"
+
+export const Heading = styled.h1(() => ({
+    fontSize: 30,
+    fontWeight: 700,
+    margin: 0,
+    fontFamily: ["Fira Code", "monospace"]
+}))
