e1f9767350696983d9d3600bbf5f2d30b7a196cb
Daniil <dadyarri@gmail.com>
2023-04-28 21:18:14 +0300

    design: update styles

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -22,7 +22,8 @@ export const Card: FC<CardProps> = ({data}) => {
         flexDirection: "row",
         boxShadow: "0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)",
         padding: "20px",
-        borderRadius: 10
+        borderRadius: 10,
+        margin: "20px"
     }}>
         <CardImage src={data.thumbnail} style={{marginRight: "20px"}}/>
         <CardBody>
