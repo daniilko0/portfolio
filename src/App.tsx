9e7479c210b29f83a9dfce37faf18580cf016baa
Daniil <dadyarri@gmail.com>
2023-04-28 16:12:31 +0300

    refactor: remove fullpage scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import {Header} from "./blocks/header";
 import {Home} from "./blocks/home";
-import ReactFullpage from '@fullpage/react-fullpage';
 import {Skills} from "./blocks/skills";
 
 function App() {
@@ -8,19 +7,9 @@ function App() {
     return (
         <>
             <Header/>
-            <ReactFullpage
-                licenseKey={"gplv3-license"}
-                scrollingSpeed={700}
-                credits={{enabled: false}}
-                render={() => {
-                    return (
-                        <ReactFullpage.Wrapper>
-                            <Home/>
-                            <Skills/>
-                        </ReactFullpage.Wrapper>
-                    );
-                }}
-            />
+
+            <Home/>
+            <Skills/>
         </>
     )
 }
