365e407e1559922dc67c557d1d854e663afe4aa5
Daniil <dadyarri@gmail.com>
2023-04-28 20:45:45 +0300

    feat: add displaying info about projects

--- src/blocks/projects/index.tsx
+++ src/blocks/projects/index.tsx
@@ -1,38 +1,14 @@
 import {Heading} from "../../components/heading";
 import {Block} from "../../components/block";
 import {Element} from "react-scroll";
-import {motion, Variants} from "framer-motion";
-
-const cardVariants: Variants = {
-    offscreen: {
-        x: -1000
-    },
-    onscreen: {
-        x: 0,
-        transition: {
-            type: "spring",
-            bounce: 0,
-            duration: 2.0
-        }
-    }
-};
+import projects from "../../data/projects.json"
+import {Card} from "../../components/card";
 
 export const Projects = () => {
     return <Element name={"projects"}>
         <Block>
             <Heading variant={"h1"}>Проекты</Heading>
-            {["red", "green", "blue", "black"].map((color) =>
-                <motion.div initial={"offscreen"} whileInView={"onscreen"}
-                            viewport={{once: true, amount: 0.8}}>
-                    <motion.div
-                        style={{
-                            background: color,
-                            width: 300,
-                            height: 300
-                        }} variants={cardVariants}>
-
-                    </motion.div>
-                </motion.div>)}
+            {projects.map(project  => <Card data={project}/>)}
         </Block>
     </Element>
 }
