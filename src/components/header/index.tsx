a5954efdb4a255379737dbf2ddf640c7b75ce472
Daniil <dadyarri@gmail.com>
2023-04-28 13:59:24 +0300

    feat: add links to blocks of the site

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,16 @@
 import {Container} from "../container";
+import {Heading} from "../heading.ts";
+import {Menu} from "../menu";
+import {MenuItem} from "../menu-item";
 
 export const Header = () => {
-    return <Container></Container>
+    return <Container>
+        <Heading style={{flexGrow: 1}}>dadyarri</Heading>
+        <Menu>
+            <MenuItem href={"#"}>Главная</MenuItem>
+            <MenuItem href={"#"}>Навыки</MenuItem>
+            <MenuItem href={"#"}>Проекты</MenuItem>
+            <MenuItem href={"#"}>Публикации</MenuItem>
+        </Menu>
+    </Container>
 }
