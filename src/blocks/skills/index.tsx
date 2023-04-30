94dfdca6ddb7fe2beb16491af0bceed74e1e3746
Daniil <dadyarri@gmail.com>
2023-04-30 18:25:14 +0300

    lint: fix warnings

--- src/blocks/skills/index.tsx
+++ src/blocks/skills/index.tsx
@@ -5,20 +5,21 @@ import {SocialLink} from "../../components/social-link";
 import {Container} from "../../components/container";
 import {Tooltip} from "react-tooltip";
 import skills from "../../data/skills.tsx";
+import {Fragment} from "react";
 
 export const Skills = () => {
     return <Element name={"skills"}>
         <Block>
             <Heading variant={"h1"}>Навыки</Heading>
             <Container>
-                {skills.map((skill) => <>
+                {skills.map((skill, index) => <Fragment key={index}>
                     <SocialLink>
-                        <a data-tooltip-id={skill.id} data-tooltip-content={skill.tooltip}>
+                        <span data-tooltip-id={skill.id} data-tooltip-content={skill.tooltip}>
                             {skill.icon}
-                        </a>
+                        </span>
                     </SocialLink>
                     <Tooltip id={skill.id} style={{fontFamily: "Fira Code"}}/>
-                </>)}
+                </Fragment>)}
             </Container>
         </Block>
     </Element>
