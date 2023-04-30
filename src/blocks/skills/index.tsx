ef9975355de988b87479f37e6213d9772b7f014f
Daniil <dadyarri@gmail.com>
2023-04-30 18:19:00 +0300

    feat: add showing skills

--- src/blocks/skills/index.tsx
+++ src/blocks/skills/index.tsx
@@ -1,11 +1,25 @@
 import {Heading} from "../../components/heading";
 import {Block} from "../../components/block";
 import {Element} from "react-scroll";
+import {SocialLink} from "../../components/social-link";
+import {Container} from "../../components/container";
+import {Tooltip} from "react-tooltip";
+import skills from "../../data/skills.tsx";
 
 export const Skills = () => {
     return <Element name={"skills"}>
         <Block>
             <Heading variant={"h1"}>Навыки</Heading>
+            <Container>
+                {skills.map((skill) => <>
+                    <SocialLink>
+                        <a data-tooltip-id={skill.id} data-tooltip-content={skill.tooltip}>
+                            {skill.icon}
+                        </a>
+                    </SocialLink>
+                    <Tooltip id={skill.id}/>
+                </>)}
+            </Container>
         </Block>
     </Element>
 }
