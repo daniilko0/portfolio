7320eb5d1297b5ace6e54f3a0a89915f2233c8f3
Daniil <dadyarri@gmail.com>
2023-04-30 18:39:18 +0300

    feat: add showing posts

--- src/blocks/posts/index.tsx
+++ src/blocks/posts/index.tsx
@@ -1,11 +1,14 @@
 import {Heading} from "../../components/heading";
 import {Block} from "../../components/block";
 import {Element} from "react-scroll";
+import posts from "../../data/posts.json";
+import {Card} from "../../components/card";
 
 export const Posts = () => {
     return <Element name={"posts"}>
         <Block>
             <Heading variant={"h1"}>Публикации</Heading>
+            {posts.map((post, index) => <Card data={post} key={index}/>)}
         </Block>
     </Element>
 }
